fa93017159c7a3b185c7a4a5b83daeebee4b439c
Gavin Mendel-Gleason <jacobian@gmail.com>
2022-09-13 22:04:45 +0200

    Fixing some schema problems, simplifying script

--- demo_data/ingest.py
+++ demo_data/ingest.py
@@ -1,115 +1,137 @@
 #!/usr/bin/env python3
 
 import jsonlines
-import pandas as pd
+import csv
+import os
+import subprocess
 
 def dump_json(writer, list_of_objs: list):
     for obj in list_of_objs:
         writer.write(obj)
 
-def ingest_minifigs():
-    minifigs_df = pd.read_csv('./minifigs.csv')
-    inserts = []
-    for _,row in minifigs_df.iterrows():
-        inserts.append({
-            '@type' : 'Minifig',
-            '@capture' : row['fig_num'],
-            'name': row['name'],
-            'img_url': row['img_url'],
-            'num_parts': row['num_parts'],
-            'figure_number' : row['fig_num']
-        })
-    return inserts
+def serialise_minifigs(output):
+    # opening the csv file
+    with open('./minifigs.csv') as csv_file:
+        # reading the csv file using DictReader
+        csv_reader = csv.DictReader(csv_file)
+        inserts = []
+        for row in csv_reader:
+            output.write({
+                '@type' : 'Minifig',
+                '@capture' : row['fig_num'],
+                'name': row['name'],
+                'img_url': row['img_url'],
+                'num_parts': int(row['num_parts']),
+                'figure_number' : row['fig_num']
+            })
 
-def ingest_inventory_minifigs():
-    inventory_minifigs_df = pd.read_csv('./inventory_minifigs.csv')
-    inserts = []
-    for _,row in inventory_minifigs_df.iterrows():
-        inserts.append({
-            '@type' : 'InventoryMinifig',
-            'quantity' : row['quantity'],
-            'minifig' : { '@ref' : row['fig_num'] }
-        })
-    return inserts
+def serialise_inventory_minifigs(output):
+    with open('./inventory_minifigs.csv') as csv_file:
+        # reading the csv file using DictReader
+        csv_reader = csv.DictReader(csv_file)
+        inserts = []
+        for row in csv_reader:
+            output.write({
+                '@type' : 'InventoryMinifig',
+                'quantity' : int(row['quantity']),
+                'minifig' : { '@ref' : row['fig_num'] }
+            })
 
-def ingest_parts():
-    parts_df = pd.read_csv('./parts.csv')
-    inserts = []
-    for _,row in parts_df.iterrows():
-        inserts.append({
-            '@type' : 'Part',
-            'part_number' : row['part_num'],
-            'category' : { '@ref' : row['part_cat_id'] },
-            'name' : row['name'],
-            'material' : 'Material'
-        })
-    return inserts
+def serialise_parts(output):
+    with open('./parts.csv') as csv_file:
+        # reading the csv file using DictReader
+        csv_reader = csv.DictReader(csv_file)
+        inserts = []
+        for row in csv_reader:
+            output.write({
+                '@type' : 'Part',
+                'part_number' : row['part_num'],
+                'category' : { '@ref' : row['part_cat_id'] },
+                'name' : row['name'],
+                'material' : 'Material'
+            })
 
-def ingest_elements(element_image_map):
-    elements_df = pd.read_csv('./elements.csv')
-    inserts = []
-    for _,row in elements_df.iterrows():
-        if row['color_id'] != -1:
-            color_obj = { '@ref' : row['color_id'] }
-        else:
-            color_obj = None
-        element_id = f"{row['part_num']} {row['color_id']}"
-        inserts.append({
-            '@type' : 'Element',
-            '@capture' : element_id,
-            'part' : row['part_num'],
-            'color' : color_obj,
-            'image_url' : element_image_map[element_id]
-        })
-    return inserts
+def serialise_elements(output,element_image_map):
+    with open('./elements.csv') as csv_file:
+        # reading the csv file using DictReader
+        csv_reader = csv.DictReader(csv_file)
+        inserts = []
+        for row in csv_reader:
+            if row['color_id'] != -1:
+                color_obj = { '@ref' : row['color_id'] }
+            else:
+                color_obj = None
+            element_id = f"{row['part_num']} {row['color_id']}"
+            if element_id in element_image_map:
+                image_url = element_image_map[element_id]
+            else:
+                image_url = None
+            output.write({
+                '@type' : 'Element',
+                '@capture' : element_id,
+                'part' : row['part_num'],
+                'color' : color_obj,
+                'image_url' : image_url
+            })
 
-def ingest_colors():
-    colors_df = pd.read_csv('./colors.csv', skiprows=[0])
-    inserts = []
-    for _,row in colors_df.iterrows():
-        inserts.append({
-            '@type' : 'Color',
-            '@capture' : row['id'],
-            'name' : row['name'],
-            'rgb' : row['rgb'],
-            'transparent' : True if row['is_trans'] == 't' else False
-        })
-    return inserts
+def serialise_colors(output):
+    with open('./colors.csv') as csv_file:
+        # reading the csv file using DictReader
+        csv_reader = csv.DictReader(csv_file)
+        inserts = []
+        for row in csv_reader:
+            output.write({
+                '@type' : 'Color',
+                '@capture' : row['id'],
+                'name' : row['name'],
+                'rgb' : row['rgb'],
+                'transparent' : True if row['is_trans'] == 't' else False
+            })
 
-def element_key(part,color_id):
-    if color_id == None:
-        color_id = '++none++'
-    part + ' ' + color_id
 
 def boolean(torf):
     return torf == 't'
 
-def ingest_inventory_parts():
-    inventory_parts_df = pd.read_csv('./inventory_parts.csv')
-    inserts = []
-    element_image_map = {}
-    for _,row in inventory_parts_df.iterrows():
-        element_id = f"{row['part_num']} {row['color_id']}"
-        image = None if pd.isna(row['img_url']) else row['img_url']
-        element_image_map[element_id] = image
-        inserts.append({
-            '@type' : 'InventoryPart',
-            '@capture' : row['inventor_id'],
-            'quantity' : row['quantity'],
-            'element' : { '@ref' : element_id},
-            'spare' : boolean(row['is_spare']),
-        })
-    return (inserts,element_image_map)
+def serialise_inventory_parts(output):
+    with open('./inventory_parts.csv') as csv_file:
+        # reading the csv file using DictReader
+        csv_reader = csv.DictReader(csv_file)
+        inserts = []
+        element_image_map = {}
+        for row in csv_reader:
+            element_id = f"{row['part_num']} {row['color_id']}"
+            image = None if row['img_url'] == '' else row['img_url']
+            element_image_map[element_id] = image
+            inserts.append({
+                '@type' : 'InventoryPart',
+                '@capture' : row['inventor_id'],
+                'quantity' : int(row['quantity']),
+                'element' : { '@ref' : element_id},
+                'spare' : boolean(row['is_spare']),
+            })
+        return element_image_map
+
+def create_db(name, cwd):
+    subprocess.call(['terminusdb', 'store', 'init', '--force'], cwd=cwd)
+    subprocess.call(['terminusdb', 'db', 'create', 'admin/lego'], cwd=cwd)
+    with open('schema.json', 'r') as f:
+        process = subprocess.Popen(['terminusdb', 'doc', 'insert', 'admin/lego', '-f', '-g', 'schema'],
+                                   cwd=cwd,
+                                   stdin=f)
+    with open('objs.json', 'r') as f:
+        process = subprocess.Popen(['terminusdb', 'doc', 'insert', 'admin/lego'],
+                                   cwd=cwd,
+                                   stdin=f)
 
 def main():
     name = 'objs.json'
     with jsonlines.open(name, mode='w') as writer:
-        dump_json(writer, ingest_minifigs())
-        dump_json(writer, ingest_inventory_minifigs())
-        dump_json(writer, ingest_parts())
-        (objs, entity_image_map) = ingest_inventory_parts()
-        dump_json(writer, objs)
-        dump_json(writer, ingest_elements(entity_image_map))
+        serialise_minifigs(writer)
+        serialise_inventory_minifigs(writer)
+        serialise_parts(writer)
+        entity_image_map = serialise_inventory_parts(writer)
+        serialise_elements(writer,entity_image_map)
+    create_db(name,'../')
 
 if __name__ == '__main__':
     main()
