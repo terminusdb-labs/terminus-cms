0a9cf2b138f11aea0663216d5f6c12cb21fdf913
Robin de Rooij <rderooij685@gmail.com>
2022-09-15 15:28:49 +0200

    Allow custom terminusdb executable with env variable

--- demo_data/ingest.py
+++ demo_data/ingest.py
@@ -130,15 +130,16 @@ def serialise_inventory_parts(output):
         return element_image_map
 
 def create_db(name, cwd):
-    subprocess.call(['terminusdb', 'store', 'init', '--force'], cwd=cwd)
-    subprocess.call(['terminusdb', 'db', 'create', 'admin/lego'], cwd=cwd)
+    terminusdb_path = 'terminusdb' if 'TERMINUSDB_EXECUTABLE' not in os.environ else os.environ['TERMINUSDB_EXECUTABLE']
+    subprocess.call([terminusdb_path, 'store', 'init', '--force'], cwd=cwd)
+    subprocess.call([terminusdb_path, 'db', 'create', 'admin/lego'], cwd=cwd)
     with open('schema.json', 'r') as f:
         process = subprocess.Popen(['terminusdb', 'doc', 'insert', 'admin/lego', '-f', '-g', 'schema'],
                                    cwd=cwd,
                                    stdin=f)
         process.wait()
     with open('objs.json', 'r') as f:
-        process = subprocess.Popen(['terminusdb', 'doc', 'insert', 'admin/lego'],
+        process = subprocess.Popen([terminusdb_path, 'doc', 'insert', 'admin/lego'],
                                    cwd=cwd,
                                    stdin=f)
         process.wait()
