cf5e63b647fa825c8abcb184a23c276e61746013
Robin de Rooij <rderooij685@gmail.com>
2022-09-19 15:18:25 +0200

    Fix some PEP issues

--- demo_data/ingest.py
+++ demo_data/ingest.py
@@ -16,13 +16,14 @@ def serialize_minifigs(output):
             num_parts = int(row['num_parts'])
             num_parts_val = num_parts if num_parts > 0 else None
             output.write({
-                '@type' : 'Minifig',
-                '@capture' : f"Minifig/{row['fig_num']}",
+                '@type': 'Minifig',
+                '@capture': f"Minifig/{row['fig_num']}",
                 'name': row['name'],
                 'img_url': row['img_url'],
                 'num_parts': num_parts_val,
-                'figure_number' : row['fig_num']
+                'figure_number': row['fig_num']
             })
+
 
 def get_part_categories():
     with open('./part_categories.csv') as csv_file:
@@ -33,18 +34,19 @@ def get_part_categories():
             part_categories[row['id']] = row['name']
         return part_categories
 
+
 def serialize_parts(output, part_categories):
     with open('./parts.csv') as csv_file:
         # reading the csv file using DictReader
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             output.write({
-                '@type' : 'Part',
-                '@capture' : f"Part/{row['part_num']}",
-                'part_number' : row['part_num'],
-                'category' : part_categories[row['part_cat_id']],
-                'name' : row['name'],
-                'material' : row['part_material']
+                '@type': 'Part',
+                '@capture': f"Part/{row['part_num']}",
+                'part_number': row['part_num'],
+                'category': part_categories[row['part_cat_id']],
+                'name': row['name'],
+                'material': row['part_material']
             })
 
 def serialize_part_relationships(output):
@@ -76,7 +78,7 @@ def serialize_elements(output, element_image_map):
         elements = {}
         for row in csv_reader:
             if row['color_id'] != -1:
-                color_obj = { '@ref' : f"Color/{row['color_id']}" }
+                color_obj = {'@ref' : f"Color/{row['color_id']}"}
             else:
                 color_obj = None
             element_id = f"{row['part_num']} {row['color_id']}"
@@ -88,13 +90,14 @@ def serialize_elements(output, element_image_map):
             else:
                 image_url = None
             output.write({
-                '@type' : 'Element',
-                '@capture' : f"Element/{element_id}",
-                'part' : { '@ref': f"Part/{row['part_num']}" },
-                'color' : color_obj,
-                'image_url' : image_url
+                '@type': 'Element',
+                '@capture': f"Element/{element_id}",
+                'part': {'@ref': f"Part/{row['part_num']}"},
+                'color': color_obj,
+                'image_url': image_url
             })
     return elements
+
 
 def serialize_colors(output):
     with open('./colors.csv') as csv_file:
@@ -102,24 +105,26 @@ def serialize_colors(output):
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             output.write({
-                '@type' : 'Color',
-                '@capture' : f"Color/{row['id']}",
-                'name' : row['name'],
-                'rgb' : row['rgb'],
-                'transparent' : True if row['is_trans'] == 't' else False
+                '@type': 'Color',
+                '@capture': f"Color/{row['id']}",
+                'name': row['name'],
+                'rgb': row['rgb'],
+                'transparent': True if row['is_trans'] == 't' else False
             })
+
 
 def serialize_themes(output):
     with open('./themes.csv') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             output.write({
-                '@type' : 'Theme',
-                '@capture' : f"Theme/{row['id']}",
+                '@type': 'Theme',
+                '@capture': f"Theme/{row['id']}",
                 'theme_id': int(row['id']),
-                'name' : row['name'],
+                'name': row['name'],
                 'parent': {'@ref': f"Theme/{row['parent_id']}"} if row['parent_id'] != '' else None
             })
+
 
 def serialize_sets(output, set_inventory_map):
     with open('./sets.csv') as csv_file:
@@ -128,10 +133,10 @@ def serialize_sets(output, set_inventory_map):
             set_id = row['set_num']
             sets = set_inventory_map[set_id] if set_id in set_inventory_map else []
             output.write({
-                '@type' : 'LegoSet',
-                '@capture' : f"LegoSet/{set_id}",
-                'theme': { "@ref": f"Theme/{row['theme_id']}" },
-                'name' : row['name'],
+                '@type': 'LegoSet',
+                '@capture': f"LegoSet/{set_id}",
+                'theme': {"@ref": f"Theme/{row['theme_id']}"},
+                'name': row['name'],
                 'year': int(row['year']),
                 'inventory_set': sets,
             })
@@ -185,6 +190,7 @@ def create_inventory_minifig_map():
             })
     return inventory_minifig_map
 
+
 def create_inventory_set_map():
     with open('./inventory_sets.csv') as csv_file:
         csv_reader = csv.DictReader(csv_file)
@@ -198,6 +204,7 @@ def create_inventory_set_map():
                 'inventory': { '@ref': f"Inventory/{row['inventory_id']}" }
             })
     return set_inventory_map
+
 
 def create_inventory_part_map(elements):
     with open('./inventory_parts.csv') as csv_file:
@@ -218,6 +225,7 @@ def create_inventory_part_map(elements):
                 'spare' : boolean(row['is_spare']),
             }
     return inventory_part_map
+
 
 def create_db(name, cwd):
     terminusdb_path = 'terminusdb' if 'TERMINUSDB_EXECUTABLE' not in os.environ else os.environ['TERMINUSDB_EXECUTABLE']
@@ -263,7 +271,8 @@ def main():
         serialize_part_relationships(writer)
     if "--no-insert" not in sys.argv:
         print("Inserting in DB")
-        create_db(name,'../')
+        create_db(name, '../')
+
 
 if __name__ == '__main__':
     main()
