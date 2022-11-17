5322172b228e2726978f470316a720cbf868b182
Kitty Jose <kitty@terminusdb.com>
2022-11-17 10:48:45 +0000

    initial try on website look and feel§

--- dashboard/src/components/ThemeComponent.js
+++ dashboard/src/components/ThemeComponent.js
@@ -4,7 +4,6 @@ import {ExecuteQuery} from "../hooks/ExecuteQuery"
 import {getThemesQuery, getLegoSetByTheme} from "../hooks/queries"
 import Card from 'react-bootstrap/Card'
 import {Row, Col, ProgressBar, Alert, Button} from 'react-bootstrap'
-import {BrowseObj} from "../hooks/BrowseContext"
 import {THEME, LEGO_SET} from "./constants"
 import {BiArrowBack} from "react-icons/bi"
 import Stack from 'react-bootstrap/Stack'
@@ -16,14 +15,14 @@ const ThemeProvider = ({themes,onNodeClick}) => {
             const name = theme.Name ? theme.Name['@value'] : ''
             const image = theme.Images ? theme.Images['@value'] : ''
             return <Col md={2} className="mb-3" key={`theme__${index}`}>
-                    <Card className="theme__card"
-                        onClick={(e) => onNodeClick(name)} >
-                        <Card.Img variant="top" src={image} />
-                        <Card.Body>
-                            <small className="fw-bold">{name}</small>
-                        </Card.Body>
-                    </Card>
-                </Col>
+                <Card className="theme__card bg-secondary"
+                    onClick={(e) => onNodeClick(name)} >
+                    <Card.Img variant="top" src={image} />
+                    <Card.Body>
+                        <small className="fw-bold">{name}</small>
+                    </Card.Body>
+                </Card>
+            </Col>
             })
 
     return <Row className="w-100 mt-5">
@@ -57,7 +56,7 @@ export const Themes = () => {
     </Alert>
 
     return <React.Fragment>
-            <h5 className="fw-bold text-muted mt-3 mb-3">
+            <h5 className="fw-bold text-muted mt-5 mb-3">
                 Browse through Themes
             </h5>
             {result && <ThemeProvider themes={result} onNodeClick={onNodeClick}/>}
--- dashboard/src/components/TopMenuWeb.js
+++ dashboard/src/components/TopMenuWeb.js
@@ -17,7 +17,7 @@ export const TopMenuWeb = ({showSearchBar=true}) => {
 				window.location.replace(`${base}`) 
 		}*/
 	 
-		return <Navbar style={{height:"80px", background:"#76b253"}} sticky="top" expand="lg" variant='dark' className="m-0 p-0 ">
+		return <Navbar style={{height:"80px", background:"#855afc"}} sticky="top" expand="lg" variant='light' className="m-0 p-0 ">
 			<Container fluid>
       			<Navbar.Brand as={RouterNavLink} to="/">
 					<span><img src={logo} className="logo-img mr-2" width="60px"/>
