0013e60600e9eb1a8125cb4df9d2f13d20dee04f
Francesca-Bit <francesca@terminusdb.com>
2022-11-24 12:35:33 +0000

    review

--- dashboard/src/utils/ApolloClientConfig.js
+++ dashboard/src/utils/ApolloClientConfig.js
@@ -4,6 +4,7 @@ import { ApolloClient,ApolloLink, concat, InMemoryCache, ApolloProvider, gql, Ht
 //local/branch/main
  //add an ENV var 
 export const createApolloClient = ()=>{
+
     const httpLink = new HttpLink({ uri: "http://localhost:6363/api/graphql/terminuscms/lego" });
     const authMiddleware = new ApolloLink((operation, forward) => {
         // add the authorization to the headers
@@ -38,4 +39,42 @@ export const createApolloClient = ()=>{
         },
         link: value,       
     });
+}
+
+export const createApolloClientWeb = ()=>{
+  
+  const httpLink = new HttpLink({ uri: "http://localhost:6363/api/graphql/terminuscms/lego" });
+  const authMiddleware = new ApolloLink((operation, forward) => {
+      // add the authorization to the headers
+      operation.setContext(({ headers = {} }) => ({
+      headers: {
+          ...headers,
+          authorization: "Basic Y29sbGFib3JhdG9yOmRlbW9fcGFzc3dvcmQ="
+      }
+      }));
+      return forward(operation);
+  })
+  
+  const cache = new InMemoryCache({
+      addTypename: false
+  });
+
+  //offsetLimitPagination
+  
+  const value = concat(authMiddleware, httpLink)
+
+  return new ApolloClient({
+      cache:cache,
+      defaultOptions: {
+        watchQuery: {
+          fetchPolicy: 'no-cache',
+          errorPolicy: 'all',
+        },
+        query: {
+          fetchPolicy: 'no-cache',
+          errorPolicy: 'all',
+        },
+      },
+      link: value,       
+  });
 }
